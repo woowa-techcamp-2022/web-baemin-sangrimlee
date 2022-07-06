b9aa60679cce79fe6a212bdd37b5b7dffc867499
sangrimlee <contact@sangrim.dev>
2022-07-06 16:15:49 +0900

    feat: 기본적인 express 구조 추가(#1)
    - express-generator를 참고하여 기본적인 express 구조 구현
    - Hello World! 출력

---
+++ src/app.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const express = require('express');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const createError = require('http-errors');
+
+const indexRouter = require('./routes/index');
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+app.use((err, req, res) => {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;

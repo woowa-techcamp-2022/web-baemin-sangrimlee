6ed28207d01ad96c94d8c81004881b8c71196ce6
sangrimlee <contact@sangrim.dev>
2022-07-07 05:06:08 +0900

    refactor: /login에서 /sign-in으로 변경
    - 회원가입을 /sign-up으로 하면서, 로그인을 /sign-in으로 변경
    - 로그인 페이지에서만 사용되는 CSS 분리

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,7 @@ router.get('/', (req, res) => {
   res.render('index', { title: 'My배민' });
 });
 
-router.get('/login', (req, res) => {
-  res.render('login', { title: '로그인' });
+router.get('/sign-in', (req, res) => {
+  res.render('sign-in', { title: '로그인' });
 });
 module.exports = router;

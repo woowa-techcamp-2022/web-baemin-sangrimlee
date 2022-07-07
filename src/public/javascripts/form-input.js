1bfc5352e9ec87ab1aba89a20632f8f1dfae957a
sangrimlee <contact@sangrim.dev>
2022-07-08 00:46:37 +0900

    refactor: form-input과 관련된 이벤트 처리 구현
    - handleFormInputEvent 추가

---
+++ src/public/javascripts/form-input.js
@@ -0,0 +1,34 @@
+export function handleFormInputEvent($element, options) {
+  const { preprocess, validate, onValidate, errorMessage } = {
+    preprocess: (value) => value,
+    validate: (value) => !!value,
+    onValidate: (isValid) => isValid,
+    errorMessage: '',
+    ...options,
+  };
+
+  const $inputElement = $element.querySelector('input');
+  const $deleteBtn = $element.querySelector('.delete-btn');
+  const $errorMessage = $element.querySelector('.form-input-error');
+
+  const onChangeValue = (value) => {
+    const processedValue = preprocess(value);
+    $inputElement.value = processedValue;
+    const isValid = validate(value);
+    $element.classList.toggle('is-valid', isValid);
+    $element.classList.toggle('is-error', !isValid);
+    $errorMessage.textContent = isValid ? '' : errorMessage;
+    onValidate(isValid);
+  };
+
+  $inputElement.addEventListener('input', (event) => {
+    onChangeValue(event.target.value);
+  });
+
+  if ($deleteBtn) {
+    $deleteBtn.addEventListener('click', () => {
+      $inputElement.value = '';
+      onChangeValue('');
+    });
+  }
+}

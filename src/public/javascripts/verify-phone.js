31042e6a87cb0860455c63d89a66fdc93272abff
sangrimlee <contact@sangrim.dev>
2022-07-07 18:41:12 +0900

    feat: 인증코드 재전송 기능 추가
    - 인증번호 재전송 기능 추가

--- src/public/javascripts/verify-phone.js
+++ src/public/javascripts/verify-phone.js
@@ -1,6 +1,20 @@
+function createVerifyCode() {
+  return Math.random().toString(10).substring(2, 6);
+}
+
+function sendVerifyCode($inputElement) {
+  setTimeout(() => {
+    $inputElement.value = createVerifyCode();
+  }, 2000);
+}
+
 function init() {
+  const $verifyPhoneForm = document.querySelector('form.verify-phone-form');
   const $phoneInput = document.querySelector('.form-input');
   const $sendCodeBtn = document.querySelector('.send-code-btn');
+  const $verifyCodeForm = document.querySelector('form.verify-code-form');
+  const $verifyCodeInput = $verifyCodeForm.querySelector('input');
+  const $resendCodeBtn = document.querySelector('.resend-code-btn');
 
   $phoneInput.addEventListener('input', (event) => {
     const currentValue = event.target.value;
@@ -13,8 +27,23 @@ function init() {
     $phoneInput.classList.toggle('is-valid', isValid);
     $sendCodeBtn.disabled = !isValid;
   });
+
   $phoneInput.querySelector('.delete-btn').addEventListener('click', () => {
     $phoneInput.querySelector('input').value = '';
+  });
+
+  $verifyPhoneForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    $sendCodeBtn.remove();
+    $verifyCodeForm.classList.remove('hidden');
+    sendVerifyCode($verifyCodeInput);
+    alert('인증번호가 발송되었습니다.');
+    $phoneInput.querySelector('input').disabled = true;
+  });
+
+  $resendCodeBtn.addEventListener('click', () => {
+    sendVerifyCode($verifyCodeInput);
+    alert('인증번호가 발송되었습니다.');
   });
 }
 

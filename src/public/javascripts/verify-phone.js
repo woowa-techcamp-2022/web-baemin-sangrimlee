a4b47726d919a186a233454923bebea181facdd6
sangrimlee <contact@sangrim.dev>
2022-07-07 17:56:52 +0900

    refactor: block-btn mixin 추가
    - 자주 사용하는 block-btn을 mixin으로 변경

--- src/public/javascripts/verify-phone.js
+++ src/public/javascripts/verify-phone.js
@@ -1,5 +1,7 @@
 function init() {
   const $phoneInput = document.querySelector('.form-input');
+  const $sendCodeBtn = document.querySelector('.send-code-btn');
+
   $phoneInput.addEventListener('input', (event) => {
     const currentValue = event.target.value;
     const processed = currentValue
@@ -7,7 +9,9 @@ function init() {
       .replace(/^(\d{0,3})(\d{0,4})(\d{0,4})$/g, '$1-$2-$3')
       .replace(/-{1,2}$/g, '');
     event.target.value = processed;
-    $phoneInput.classList.toggle('is-valid', processed.length === 13);
+    const isValid = processed.length === 13;
+    $phoneInput.classList.toggle('is-valid', isValid);
+    $sendCodeBtn.disabled = !isValid;
   });
   $phoneInput.querySelector('.delete-btn').addEventListener('click', () => {
     $phoneInput.querySelector('input').value = '';

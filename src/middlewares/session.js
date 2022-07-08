110c83955adeef51690b6ebc5d4c686bdd7e8743
sangrimlee <contact@sangrim.dev>
2022-07-08 09:15:52 +0900

    fix: 쿠키가 없는 경우에 무한 로딩되는 문제 해결

--- src/middlewares/session.js
+++ src/middlewares/session.js
@@ -3,16 +3,13 @@ const { findUserById } = require('../db/user');
 
 function session(req, res, next) {
   const sid = req.cookies['sid'];
-  if (sid) {
-    getSession(sid, (getSessionError, session) => {
-      if (session) {
-        findUserById(session.uid, (findUserByIdError, user) => {
-          req.session = user;
-          next();
-        });
-      }
+
+  getSession(sid, (getSessionError, session) => {
+    findUserById(session?.uid, (findUserByIdError, user) => {
+      req.session = user;
+      next();
     });
-  }
+  });
 }
 
 module.exports = session;

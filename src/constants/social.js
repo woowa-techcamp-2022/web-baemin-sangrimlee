b61bc347e106a1a2f6d21b346bc4880db340027f
sangrimlee <contact@sangrim.dev>
2022-07-11 08:35:14 +0900

    feat: 로그인 페이지 모두 구현

---
+++ src/constants/social.js
@@ -0,0 +1,18 @@
+const SOCIAL_LIST = [
+  {
+    label: '페이스북으로 로그인',
+    icon: 'facebook',
+  },
+  {
+    label: '네이버로 로그인',
+    icon: 'naver',
+  },
+  {
+    label: 'Apple로 로그인',
+    icon: 'apple',
+  },
+];
+
+module.exports = {
+  SOCIAL_LIST,
+};

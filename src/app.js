3e0ab2587efb9db776ae094bbabfefcbcc181003
sangrimlee <contact@sangrim.dev>
2022-07-10 16:41:37 +0900

    feat: 404 페이지 추가
    - 404 페이지 추가

--- src/app.js
+++ src/app.js
@@ -24,12 +24,4 @@ app.use((req, res, next) => {
   next(createError(404));
 });
 
-app.use((err, req, res) => {
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  res.status(err.status || 500);
-  res.render('404');
-});
-
 module.exports = app;

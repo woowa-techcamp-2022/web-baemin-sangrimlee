3b130cc277b9b74853985ccb7436b200307c7d56
sangrimlee <contact@sangrim.dev>
2022-07-11 08:44:10 +0900

    refactor: page별 css 파일 분리 및 디렉토리 구조 변경

---
+++ src/constants/agree.js
@@ -0,0 +1,31 @@
+const AGREE_LIST = [
+  {
+    id: 'agree-1',
+    label: '배달의민족 이용약관 동의',
+    required: true,
+  },
+  {
+    id: 'agree-2',
+    label: '전자금융거래 이용약관 동의',
+    required: true,
+  },
+  {
+    id: 'agree-3',
+    label: '개인정보 수집이용 동의',
+    required: true,
+  },
+  {
+    id: 'agree-4',
+    label: '개인정보 제3자 제공 동의',
+    required: false,
+  },
+  {
+    id: 'agree-5',
+    label: '마케팅 정보 메일, SMS 수신동의',
+    required: false,
+  },
+];
+
+module.exports = {
+  AGREE_LIST,
+};
--- src/routes/auth/sign-up.js
+++ src/routes/auth/sign-up.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const { AGREE_LIST } = require('../../constants/agree');
+
 const router = express.Router();
 
 router.get('/', (req, res) => {
@@ -11,33 +13,7 @@ router.get('/agree', (req, res) => {
     headerTitle: '회원가입',
     headerRightLink: 'close',
     headerRightHref: '/',
-    agreeList: [
-      {
-        id: 'agree-1',
-        label: '배달의민족 이용약관 동의',
-        required: true,
-      },
-      {
-        id: 'agree-2',
-        label: '전자금융거래 이용약관 동의',
-        required: true,
-      },
-      {
-        id: 'agree-3',
-        label: '개인정보 수집이용 동의',
-        required: true,
-      },
-      {
-        id: 'agree-4',
-        label: '개인정보 제3자 제공 동의',
-        required: false,
-      },
-      {
-        id: 'agree-5',
-        label: '마케팅 정보 메일, SMS 수신동의',
-        required: false,
-      },
-    ],
+    agreeList: AGREE_LIST,
   });
 });
 

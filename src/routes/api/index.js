073c3f43918b60a6ed88f1fecff583230d0c7077
sangrimlee <contact@sangrim.dev>
2022-07-11 08:52:49 +0900

    feat: 이메일 중복 확인 API 추가

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { signUp, signIn } = require('../../controllers/auth');
+const { signUp, signIn, checkEmail } = require('../../controllers/auth');
 
 const router = express.Router();
 
 router.post('/sign-up', signUp);
 
 router.post('/sign-in', signIn);
+
+router.get('/check-email', checkEmail);
 
 module.exports = router;
